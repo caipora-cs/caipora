a3f921b1f3bc1a1d07fcce2106635a67082b015d
caipora-cs <0xaoi@protonmail.com>
2023-06-27 14:30:53 +0100

    Three.js refactoring

--- src/systems/renderer.ts
+++ src/systems/renderer.ts
@@ -1,10 +1,10 @@
-import { sRGBEncoding, WebGLRenderer } from "three";
+import { WebGLRenderer } from "three";
 
 function createRenderer() {
   const renderer = new WebGLRenderer({ antialias: true, alpha: true });
 
-  renderer.physicallyCorrectLights = true;
-  renderer.outputEncoding = sRGBEncoding;
+  // renderer.physicallyCorrectLights = true;
+  // renderer.outputEncoding = sRGBEncoding;
   renderer.shadowMap.enabled = true;
 
   return renderer;
